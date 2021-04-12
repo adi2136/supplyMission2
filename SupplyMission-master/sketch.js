d5759c72224fb23025eef8ed072da7f0335fa70c
adi2136 <82223826+adi2136@users.noreply.github.com>
2021-04-12 20:25:07 +0530

    Add files via upload

---
+++ SupplyMission-master/sketch.js
@@ -0,0 +1,87 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var kt;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+	
+	
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	kt=createSprite(400, 650, 200,20);
+	kt.shapeColor=color("red")
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+	 boxPosition=width/2-100
+	 boxy=610
+	 boxLeftSprite=createSprite(boxPosition, boxy, 20, 100 );
+ 	boxLeftSprite.shapeColor=("red")
+	 
+	 boxLeftBodies = Bodies.rectangle(boxPosition+20,boxy, 20, 100, {isStatic:true} );
+ 	World.add(world, boxLeftBodies);
+	 boxBase= Bodies.rectangle(boxPosition+100,boxy+40, 200, 20, {isStatic:true} ); 
+	 World.add(world, boxBase);
+	 boxBottomBodies = Bodies.rectangle(boxPosition+100,boxy+45-20, 200, 20, {isStatic:true} );
+	 World.add(world, boxBottomBodies);
+	
+	 boxLeftSprite = createSprite(boxPosition+200,boxy, 20, 100 );
+	 boxLeftSprite.shapeColor=color("red")
+	 boxRightBodies = Bodies.rectangle(boxPosition+200-20,boxy, 20, 100, {isStatic:true} );
+	 World.add(world, boxRightBodies);
+
+	Engine.run(engine); 
+
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite .collide(groundSprite)
+	
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody,false)
+  }
+}
+
+
+
